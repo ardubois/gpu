a27d9807b67385c7efab14034bed0609390c361a
ardubois <rauberdubois@gmail.com>
2023-05-16 19:11:15 -0300

    first version

--- lib/gpu.ex
+++ lib/gpu.ex
@@ -4,6 +4,49 @@ defmodule GPU do
       :erlang.load_nif('./priv/gpu_nifs', 0)
       #IO.puts("ok")
   end
+  defp gen_para(p,:matrex) do
+    "float *#{p}"
+  end
+  defp gen_para(p,:float) do
+    "float #{p}"
+  end
+  defp gen_para(p,:int) do
+    "int #{p}"
+  end
+  defmacro kernel(header, do: body) do
+      {fname, _, para} = header
+     {param_list,types} = if is_list(List.last(para)) do
+        types = List.last(para)
+        param_list = para
+          |> List.delete_at(length(para)-1)
+          |> Enum.map(fn({p, _, _}) -> p end)
+          |> Enum.zip(types)
+          |> Enum.map(fn({p,t}) -> gen_para(p,t) end)
+          |> Enum.join(", ")
+        {param_list,types}
+      else
+      types = List.duplicate(:matrex,length(para))
+      param_list = para
+      |> Enum.map(fn({p, _, _}) -> p end)
+      |> Enum.zip(types)
+      |> Enum.map(fn({p,t}) -> gen_para(p,t) end)
+      |> Enum.join(", ")
+      {param_list,types}
+     end
+     cuda_body = GPU.CudaBackend.gen_body(body)
+     k = GPU.CudaBackend.gen_kernel(fname,param_list,cuda_body)
+     accessfunc = GPU.CudaBackend.gen_kernel_call(fname,length(types),Enum.reverse(types))
+     file = File.open!("c_src/#{fname}.cu", [:write])
+     IO.write(file, "#include \"erl_nif.h\"\n\n" <> k <> "\n\n" <> accessfunc)
+     File.close(file)
+     #IO.puts k
+     #IO.puts accessfunc
+     quote do
+        def unquote(header)do
+          IO.puts "hello"
+        end
+      end
+  end
   def create_ref_nif(_matrex) do
     raise "NIF create_ref_nif/1 not implemented"
 end
@@ -13,6 +56,12 @@ def create_ref(%Matrex{data: matrix} = a) do
 end
 def new_ref_nif(_matrex) do
   raise "NIF new_ref_nif/1 not implemented"
+end
+def synchronize_nif() do
+  raise "NIF new_ref_nif/1 not implemented"
+end
+def synchronize() do
+  synchronize_nif()
 end
 def new_ref(size) do
 ref=new_ref_nif(size)
@@ -30,6 +79,18 @@ def load_kernel_nif(_matrex) do
 end
 def kernel(s) do
   s
+end
+def build(kernelname) do
+  {result, _errcode} = System.cmd("nvcc",
+      ["--shared",
+        "--compiler-options",
+        "'-fPIC'",
+        "-o",
+        "priv/#{kernelname}.so",
+        "c_src/#{kernelname}.cu"
+      ], stderr_to_stdout: true)
+  IO.puts(result)
+  GPU.load_kernel_nif(kernelname)
 end
 def build_kernel(kernel,kernelname,nargs,types) do
   accessfunc = GPU.Backend.gen_c_kernel(kernelname,nargs,Enum.reverse(types))
