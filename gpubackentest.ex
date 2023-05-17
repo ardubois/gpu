f28c4b160eb0df5257602dc8b1074a5d1a550c34
ardubois <rauberdubois@gmail.com>
2023-05-17 15:57:20 -0300

    completed the example

--- gpubackentest.ex
+++ gpubackentest.ex
@@ -1,17 +1,5 @@
 
-k = GPU.kernel "__global__
-void add_vectors(float *result, float *a, float *b, int N)
-{
-  int index = threadIdx.x + blockIdx.x * blockDim.x;
-  int stride = blockDim.x * gridDim.x;
 
-  for(int i = index; i < N; i += stride)
-  {
-    result[i] = a[i] + b[i];
-  }
-
-}
-"
 defmodule MyKernel do
   import GPU
 #@spec myk(integer,integer,integer,integer)::none
@@ -41,5 +29,7 @@ ref3=GPU.new_ref(5)
 kernel=GPU.build('aadd_vectors')
 IO.puts("teste ok")
 GPU.spawn(kernel,10,10,[ref3,ref1,ref2,666])
-
+GPU.synchronize()
+result = GPU.get_matrex(ref3)
+IO.inspect result
 #IO.puts GPU.Backend.gen_c_kernel('addVectors',4,[])
