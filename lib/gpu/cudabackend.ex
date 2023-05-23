7e41fd3c25d4601272bda419ea6e1accda70b650
ardubois <rauberdubois@gmail.com>
2023-05-23 09:22:55 -0300

    added more arguments to thread and blocks

--- lib/gpu/cudabackend.ex
+++ lib/gpu/cudabackend.ex
@@ -119,12 +119,31 @@ defmodule GPU.CudaBackend do
     ERL_NIF_TERM list;
     ERL_NIF_TERM head;
     ERL_NIF_TERM tail;
-
-    int blocks,threads;
     float **array_res;
 
-    enif_get_int(env, argv[1], &blocks);
-    enif_get_int(env, argv[2], &threads);
+    const ERL_NIF_TERM *tuple_blocks;
+    const ERL_NIF_TERM *tuple_threads;
+    int arity;
+
+    if (!enif_get_tuple(env, argv[1], &arity, &tuple_blocks)) {
+      printf (\"spawn: blocks argument is not a tuple\");
+    }
+
+    if (!enif_get_tuple(env, argv[2], &arity, &tuple_threads)) {
+      printf (\"spawn:threads argument is not a tuple\");
+    }
+    int b1,b2,b3,t1,t2,t3;
+
+    enif_get_int(env,tuple_blocks[0],&b1);
+    enif_get_int(env,tuple_blocks[1],&b2);
+    enif_get_int(env,tuple_blocks[2],&b3);
+    enif_get_int(env,tuple_threads[0],&t1);
+    enif_get_int(env,tuple_threads[1],&t2);
+    enif_get_int(env,tuple_threads[2],&t3);
+
+    dim3 blocks(b1,b2,b3);
+    dim3 threads(t1,t2,t3);
+
     list= argv[3];
 
 "
