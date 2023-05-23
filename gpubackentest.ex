4d71f2034e1b21ebeef20bc6ddf4ca6c7ac0f7d7
ardubois <rauberdubois@gmail.com>
2023-05-23 09:23:23 -0300

    testing new interface for spawn

--- gpubackentest.ex
+++ gpubackentest.ex
@@ -28,7 +28,7 @@ ref3=GPU.new_ref(5)
 #IO.gets("asdf")
 kernel=GPU.build('aadd_vectors')
 IO.puts("teste ok")
-GPU.spawn(kernel,10,10,[ref3,ref1,ref2,5])
+GPU.spawn(kernel,{10,11,12},{20,21,22},[ref3,ref1,ref2,5])
 GPU.synchronize()
 result = GPU.get_matrex(ref3)
 IO.inspect result
