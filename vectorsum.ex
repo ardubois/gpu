2bca17df8dd39dd669997c12d341fe97f034c43f
ardubois <rauberdubois@gmail.com>
2023-05-25 15:32:49 -0300

    complemented the example

--- vectorsum.ex
+++ vectorsum.ex
@@ -3,7 +3,7 @@
 defmodule MyKernel do
   import GPU
 #@spec myk(integer,integer,integer,integer)::none
-kernel aadd_vectors(result, a, b, n, [:matrex,:matrex,:matrex,:int]) do
+kernel add_vectors(result, a, b, n, [:matrex,:matrex,:matrex,:int]) do
   var index int = threadIdx.x + blockIdx.x * blockDim.x;
   var stride int = blockDim.x * gridDim.x;
   for i in range(index,n,stride) do
@@ -12,24 +12,35 @@ kernel aadd_vectors(result, a, b, n, [:matrex,:matrex,:matrex,:int]) do
 end
 end
 
+n = 1000
 
-list = [[1,2,3,4,5]]
+list = [Enum.to_list(1..n)]
 
-mat = Matrex.new(list)
-#IO.inspect(mat)
-#IO.puts("yeah1")
-ref1=GPU.create_ref(mat)
-ref2=GPU.create_ref(mat)
-ref3=GPU.new_ref(5)
-#IO.puts("yeah2")
-#matrex = GPU.get_matrex(ref)
-#IO.inspect(matrex)
-#TesteGPU.printMatrex(mat)
-#IO.gets("asdf")
-kernel=GPU.build('aadd_vectors')
-IO.puts("teste ok")
-GPU.spawn(kernel,{10,1,1},{10,1,1},[ref3,ref1,ref2,5])
+vet1 = Matrex.new(list)
+vet2 = Matrex.new(list)
+
+IO.inspect vet1
+
+ref1=GPU.create_ref(vet1)
+ref2=GPU.create_ref(vet2)
+ref3=GPU.new_ref(n)
+
+kernel=GPU.build('add_vectors')
+
+threadsPerBlock = 128;
+numberOfBlocks = div(n + threadsPerBlock - 1, threadsPerBlock)
+
+prev = System.monotonic_time()
+GPU.spawn(kernel,{numberOfBlocks,1,1},{threadsPerBlock,1,1},[ref3,ref1,ref2,5])
 GPU.synchronize()
+next = System.monotonic_time()
+IO.puts "time gpu #{System.convert_time_unit(next-prev,:native,:millisecond)}"
+
 result = GPU.get_matrex(ref3)
 IO.inspect result
-#IO.puts GPU.Backend.gen_c_kernel('addVectors',4,[])
+
+
+prev = System.monotonic_time()
+eresult = Matrex.add(vet1,vet2)
+next = System.monotonic_time()
+IO.puts "time cpu #{System.convert_time_unit(next-prev,:native,:millisecond)}"
